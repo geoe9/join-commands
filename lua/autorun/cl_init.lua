f5ee45658083f24d3af461d437d707fed1231565
George <48719453+geoe9@users.noreply.github.com>
2020-05-07 00:57:49 +0100

    Update cl_init.lua

--- lua/autorun/cl_init.lua
+++ lua/autorun/cl_init.lua
@@ -3,6 +3,12 @@
 -- @author      geo
 -- @build       v1.0
 ----------------------------------
+config = {
+    {"cl_threaded_bone_setup", "1"},
+    {"gmod_mcore_test", "1"},
+    {"mat_queue_mode", "-1"},
+    {"mat_specular", "0"}
+}
 net.Receive("RunJoinCommands",function(len)
 	for i=1, #config do
 		RunConsoleCommand(config[1], config[2])
